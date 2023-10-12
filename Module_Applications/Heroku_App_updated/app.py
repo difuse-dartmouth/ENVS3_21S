ed86262806016dcae8d1b4792422ef97cded723e
Rahul Gupta <115047275+Sciotaku@users.noreply.github.com>
2023-10-12 11:41:44 -0400

    Update app.py

--- Module_Applications/Heroku_App_updated/app.py
+++ Module_Applications/Heroku_App_updated/app.py
@@ -21,6 +21,17 @@ from statsmodels.stats.anova import anova_lm
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+# https://stackoverflow.com/questions/71106940/cannot-import-name-centered-from-scipy-signal-signaltools
+def _centered(arr, newsize):
+    # Return the center newsize portion of the array.
+    newsize = np.asarray(newsize)
+    currsize = np.array(arr.shape)
+    startind = (currsize - newsize) // 2
+    endind = startind + newsize
+    myslice = [slice(startind[k], endind[k]) for k in range(len(endind))]
+    return arr[tuple(myslice)]
+
+scipy.signal.signaltools._centered = _centered
 
 #import data from csv
 df = pd.read_csv('https://raw.githubusercontent.com/difuse-dartmouth/21s_ENVS3/main/Module_Data/parish_data_v7.csv')
